e82b17cc66d0d6ebcc178f0369b93ebff3e148c1
Vedant <vedantkumar.007@gmail.com>
2020-04-06 01:52:16 -0500

    source code

---
+++ udacity-c2-restapi/www/config/config.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// export const config = {
+//   "dev": {
+//     "username": process.env.POSTGRESS_USERNAME,
+//     "password": process.env.POSTGRESS_PASSWORD,
+//     "database": process.env.POSTGRESS_DATABASE,
+//     "host": process.env.POSTGRESS_HOST,
+//     "dialect": "postgres",
+//     "aws_region": process.env.AWS_REGION,
+//     "aws_profile": process.env.AWS_PROFILE,
+//     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+//   },
+//   "prod": {
+//     "username": "",
+//     "password": "",
+//     "database": "udagram_prod",
+//     "host": "",
+//     "dialect": "postgres"
+//   }
+// }
+exports.config = {
+    "postgress": {
+        "username": process.env.POSTGRESS_USERNAME,
+        "password": process.env.POSTGRESS_PASSWORD,
+        "database": process.env.POSTGRESS_DATABASE,
+        "host": process.env.POSTGRESS_HOST,
+        "dialect": "postgres",
+    },
+    "aws": {
+        "aws_region": process.env.AWS_REGION,
+        "aws_profile": process.env.AWS_PROFILE,
+        "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    },
+    "jwt": {
+        "secret": "helloworld"
+    }
+};
+//# sourceMappingURL=config.js.map
